f9ac9e52cfd177c5a7054a016d7db87c52dfac83
Sandeep Panigrahi <156462298+Blessedsan123@users.noreply.github.com>
2025-07-16 20:08:10 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -12,14 +12,22 @@ module tb;
   reg clk;
   reg rst_n;
   reg ena;
+  reg [7:0] ui_in;
+  reg [7:0] uio_in;
   wire [7:0] uo_out;
+  wire [7:0] uio_out;
+  wire [7:0] uio_oe;
 
   // Instantiate the top-level wrapper
   tt_um_myprocessor dut (
     .clk(clk),
     .rst_n(rst_n),
     .ena(ena),
-    .uo_out(uo_out)
+    .uo_out(uo_out),
+    .ui_in  (ui_in),    // Dedicated inputs
+    .uio_in (uio_in),   // IOs: Input path
+    .uio_out(uio_out),  // IOs: Output path
+    .uio_oe (uio_oe),
   );
 
   // Clock generation: 10ns period (100 MHz)
